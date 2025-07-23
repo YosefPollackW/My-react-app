57148f66698d3b9ecf6a568b93068e8bf8a668fb
YosefPollackW <yosefpollackw@gmail.com>
2025-07-23 11:31:24 +0300

    Initial commit

---
+++ dummyApp/src/components/Product.jsx
@@ -0,0 +1,11 @@
+
+function Product({ title, description }) {
+  return (
+    <div className="product">
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Product;
