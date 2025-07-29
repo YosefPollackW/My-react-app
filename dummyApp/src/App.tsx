d2c9b9266f03c4d7d97ed6610436a90029d5f592
YosefPollackW <yosefpollackw@gmail.com>
2025-07-29 10:28:24 +0300

    errased unnecessary lines

--- dummyApp/src/App.tsx
+++ dummyApp/src/App.tsx
@@ -1,46 +1,11 @@
 import './App.css';
-// import Product from './components/Product';
 import ProductGallery from './components/ProductGallery'
-import { useState, useEffect } from "react";
-
-interface ProductType {
-  id: number;
-  title: string;
-  description: string;
-  thumbnail: string;
-}
-
-interface ApiResponse {
-  products: ProductType[];
-}
 
 function App() {
-  // const [json, setJson] = useState<ProductType[]>([]);
-
-  // useEffect(() => {
-  //   fetch('https://dummyjson.com/products')
-  //     .then(res => res.json())
-  //     .then((data: ApiResponse) => {
-  //       console.log(data);
-  //       setJson(data.products);
-  //     })
-  //     .catch(err => console.error("Error fetching data:", err));
-  // }, []);
-
   return (
     <div className="App">
       <h1>Product List</h1>
       <ProductGallery/>
-      {/* <div className='products'>
-        {json.map((product) => (
-          <Product
-            key={product.id}
-            title={product.title}
-            description={product.description}
-            image={product.thumbnail}
-          />
-        ))}
-      </div> */}
     </div>
   );
 }
